3e663c20faf7f0cd7a7c793e7bc3bbf8142d35ad
JFVela <jfvela_2012@hotmail.com>
2024-04-25 02:44:59 -0500

    ga

--- views/inicio.php
+++ views/inicio.php
@@ -1,11 +1,149 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Andes</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="../assents/css/inicio.css">
 </head>
+
 <body>
-    <h1>Hola mundo</h1>
+    <header class="header">
+        <!-- EL MENU PRINCIPAL-->
+        <div class="menu container">
+            <a href="#" class="logo">CineAndes</a>
+            <input type="checkbox" id="menu" />
+            <label for="menu">
+                <img src="../assents/imag/Icono.png" class="menu-icono" alt="menu">
+            </label>
+
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">Nosotros</a></1i>
+                    <li><a href="#">Pe1icu1as</a></1i>
+                    <li><a href="#">Contacto</a></1i>
+                    <li><a href="#">Preguntas</a></1i>
+                </ul>
+            </nav>
+            <a href="login.php" class="btn-l">Login</a>
+        </div>
+
+        <!-- CONTENIDO -->
+        <div class="header-content container">
+            <div class="header-1">
+                <img src="../assents/imag/PROTOTIPO.jpeg" alt="">
+                <a href="#" class="btn-2">Ver ahora</a>
+            </div>
+            <div class="header-2">
+                <h1>Las mejores <br> Peliculas </h1>
+                <img src="../assents/imag/Play.png" alt="">
+            </div>
+        </div>
+
+    </header>
+
+    <!-- SECCION DE PELICULAS -->
+    <section class="movies container">
+        <h2>Peliculas mas vistas</h2>
+        <hr>
+        <div class="box-container-1">
+            <div class="box-1">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+            <div class="box-1">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+            <div class="box-1">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+            <div class="box-1">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="load-more" id="load-more-1"> Cargar mas</div>
+    </section>
+
+    <!-- Peliculas de Accion-->
+    <section class="movies container">
+        <h2>Peliculas de accion</h2>
+        <hr>
+        <div class="box-container-2">
+            <div class="box-2">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+            <div class="box-2">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+            <div class="box-2">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+            <div class="box-2">
+                <div class="content">
+                    <img src="../assents/imag/fondoLogin.jpeg" alt="">
+                    <h3>Pelicula en HD</h3>
+                    <p>
+                        Este es mi primer avance de nuestro trabajo de innovación.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="load-more" id="load-more-2"> Cargar mas</div>
+    </section>
+
+    <footer class="footer container">
+        <h3>CineAndes</h3>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Peliculas</a></li>
+            <li><a href="#">Contactos</a></li>
+        </ul>
+    </footer>
+
+    <script src=""></script>
 </body>
+
 </html>
