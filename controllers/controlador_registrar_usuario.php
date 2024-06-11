e030967987c0a93662f08b7dd3f2541166789f03
JFVela <jfvela_2012@hotmail.com>
2024-06-11 14:17:29 -0500

    Mejorando el Registrar usuario

--- controllers/controlador_registrar_usuario.php
+++ controllers/controlador_registrar_usuario.php
@@ -1,31 +1,15 @@
 <?php
 include "../config/conexion.php";
-if (!empty($_POST["registro"])) {
-    $primerNombre = $_POST["firstName"];
-    $segundoNombre = $_POST["secondName"];
-    $apellidoPaterno = $_POST["lastName1"];
-    $apellidoMaterno = $_POST["lastName2"];
-    $correoElectronico = $_POST["email"];
-    $telefono = $_POST["phone"];
-    $contrasena = $_POST["password"];
-    $confirmarContrasena = $_POST["confirmPassword"];
+if (!empty($_POST["btnRegistrar"])) {
 
-    if ($contrasena != $confirmarContrasena) {
-        echo '<div class="alert">Las contraseñas no coinciden</div>';
-        exit();
-    } else {
-        $contraseñaHash = password_hash($contrasena, PASSWORD_DEFAULT);
-        $sql = $conexion->query("INSERT INTO cineandes.usuarios (nomb1, nomb2, ape1, ape2, telefono, correo, contraseña, fecha_registro) VALUES ('$primerNombre', '$segundoNombre', '$apellidoPaterno', '$apellidoMaterno', '$telefono', '$correoElectronico', '$contraseñaHash', NOW())");
-        if ($sql == 1) {
-            echo '<div class="alert">Agregado correctamente</div>';
-        } else {
-            echo '<div class="alert">
-            Error al registrar 
-            <br> 
-            El correo: "' . $correoElectronico . '", ya existe.
-            </div>';
-        }
-    }
+    $ObtenerNombreUsuario = $_POST["nombreUsuario"];
+    $ObtenerTelefono = $_POST["phone"];
+    $ObtenerCorreo = $_POST["email"];
+    $ObtenerContraseña = $_POST["password"];
+    $ObtenerVerificación= $_POST["confirmPassword"];
+
+
+
 }
 
 ?>
--- views/createLogin.php
+++ views/createLogin.php
@@ -18,84 +18,83 @@ include '../controllers/controlador_registrar_usuario.php';
     <title>Registrar</title>
     <!--ESTILOS-->
     <link rel="stylesheet" href="../assents/css/login.css">
+
 </head>
 
 <body>
-    <section class="h-100 gradient-form" style="background-color: #eee;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
-                    <div class="card rounded-3 text-black">
-                        <div class="row g-0">
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2" style="background-image: url('../assents/imag/fondoLogin.jpeg');">
-                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                    <h4 class="mb-4">We are more than just a company</h4>
-                                    <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="card-body p-md-5 mx-md-4">
 
-                                    <div class="text-center">
-                                        <img src="../assents/imag/logoAndes.png" style="width: 185px;" alt="logo">
-                                        <h4 class="mt-1 mb-5 pb-1">Porfavor complete el formulario</h4>
+    <!--HEADER-->
+    <?php
+    include "../includes/header.php";
+    ?>
+    <!--FIN HEADER-->
+    <div class="d-flex flex-column min-vh-100">
+        <section class="section_login flex-grow-1 gradient-form">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-xl-10">
+                        <div class="card rounded-3 text-black">
+                            <div class="row g-0">
+                                <div class="pequeña_Imagen col-lg-6 d-flex align-items-center gradient-custom-2">
+                                    <div class="px-3 py-4 p-md-5 mx-md-4" style="background-color: #ffe6cc; border-radius: 15px;">
+                                        <h4 class="mb-4" style="color: #ff6600; font-size: 24px;">¡Registrate ahora!</h4>
+                                        <p class="small mb-0" style="color: #333333; font-size: 18px;">Disfruta del mejor catalogo de peliculas totalmente gratis. Ademas de poder realizar donaciiones a los mas necesitados.</p>
                                     </div>
+                                </div>
 
-                                    <!-- Formulario para regsitrar -->
-                                    <form action="" method="post" class="registrarUsuario" onsubmit="return validarContraseña();">
-                                        <div class="row">
-                                            <div class="col">
-                                                <label for="firstName" class="form-label">Primer Nombre</label>
-                                                <input type="text" class="form-control" id="firstName" name="firstName" required>
+                                <div class="col-lg-6">
+                                    <div class="card-body p-md-5 mx-md-4">
+                                        <div class="text-center">
+                                            <img src="../assents/imag/logoAndes.png" style="width: 185px;" alt="logo">
+                                            <h4 class="mt-1 mb-5 pb-1">Donde el entretenimiento encuentra su hogar en las alturas de los Andes</h4>
+                                        </div>
+                                        <form action="" method="post" class="registrarUsuario">
+                                            <p>Crea un nombre de Usuario</p>
+                                            <div class="form-outline mb-4">
+                                                <label for="nombreUsuario" class="form-label">Nombre de Usuario</label>
+                                                <input type="text" class="form-control" id="idNombreUsuario" name="nombreUsuario" required>
                                             </div>
-                                            <div class="col">
-                                                <label for="secondName" class="form-label">Segundo Nombre</label>
-                                                <input type="text" class="form-control" id="secondName" name="secondName" required>
+                                            <div class="form-outline mb-4">
+                                                <label for="phone" class="form-label">Número de Teléfono</label>
+                                                <input type="tel" class="form-control" id="IdPhone" name="phone" placeholder="Opcional" pattern="9[0-9]{8}|^$">
                                             </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col">
-                                                <label for="lastName1" class="form-label">Apellido Paterno</label>
-                                                <input type="text" class="form-control" id="lastName1" name="lastName1" required>
+                                            <div class="form-outline mb-4">
+                                                <label for="email" class="form-label">Correo Electrónico</label>
+                                                <input type="email" class="form-control" id="IdEmail" name="email" required>
                                             </div>
-                                            <div class="col">
-                                                <label for="lastName2" class="form-label">Apellido Materno</label>
-                                                <input type="text" class="form-control" id="lastName2" name="lastName2" required>
+                                            <div class="form-outline mb-4">
+                                                <label for="password" class="form-label">Contraseña</label>
+                                                <input type="password" class="form-control" id="IdPassword" name="password" required>
                                             </div>
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="email" class="form-label">Correo Electrónico</label>
-                                            <input type="email" class="form-control" id="email" name="email" required>
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="phone" class="form-label">Número de Teléfono (Opcional)</label>
-                                            <input type="tel" class="form-control" id="phone" name="phone" required>
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="password" class="form-label">Contraseña</label>
-                                            <input type="password" class="form-control" id="password" name="password" required>
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="confirmPassword" class="form-label">Confirmar Contraseña</label>
-                                            <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
-                                        </div>
-                                        <div class="text-center pt-1 mb-5 pb-1 cuenta">
-                                            <input type="submit" class="btn btn-primary" value="Registrar" name="registro"></input>
-                                            <a href="login.php">Iniciar sección</a>
-                                        </div>
-                                    </form>
+                                            <div class="form-outline mb-4">
+                                                <label for="confirmPassword" class="form-label">Confirmar Contraseña</label>
+                                                <input type="password" class="form-control" id="IdConfirmPassword" name="confirmPassword" required>
+                                            </div>
+                                            <div class="text-center pt-1 mb-5 pb-1">
+                                                <input class="btn_ingresar btn btn-block fa-lg gradient-custom-2 mb-3" type="submit" name="btnRegistrar" value="Registrarse Ahora!">
+                                                <br>
+                                                <p>Al registrarte, aceptas nuestras Condiciones de uso y Politicas de privacidad.</p>
+                                                <p>
+                                                    ¿Ya tienes una cuenta?
+                                                    <a class="text-muted" href="login.php">Iniciar Sesión</a>
+                                                </p>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <!--FOOTER-->
+        <?php
+        include "../includes/footer.php";
+        ?>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="../assents/script/validarContraseña.js"></script>
 </body>
 
 </html>
